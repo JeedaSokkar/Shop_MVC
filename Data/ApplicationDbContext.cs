37e2c1e30e466c78ae066772f6e51523ce01f9b9
JeedaSokkar <jeedasokkar2003@gmail.com>
2026-05-31 11:40:57 +0300

    add views

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShopMvc.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ShopMvc;Trusted_Connection=True;");
+
+        }
+    }
+}
